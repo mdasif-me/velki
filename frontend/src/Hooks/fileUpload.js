733201327b2246df04ac13d35cb15af478dc1693
Muhammad Asif <96514685+muhammadasif-cse@users.noreply.github.com>
2025-04-11 15:52:47 +0600

    Update fileUpload.js

--- frontend/src/Hooks/fileUpload.js
+++ frontend/src/Hooks/fileUpload.js
@@ -1,18 +1,19 @@
+import {ServerAPI} from "../API/ServerAPI";
 
 
 export const fileUpload = async (formData, setUploadURL) => {
-  const uploadURL = `http://localhost:5000/api/v1/upload/single-upload`;
+    const uploadURL = `${ServerAPI}/upload/single-upload`;
 
-  fetch(uploadURL, {
-    method: "POST",
-    body: formData,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Data: ", data);
-      if (data.status === "success") {
-        console.log("Response:", data);
-        setUploadURL(data.url);
-      }
-    });
+    fetch(uploadURL, {
+        method: "POST",
+        body: formData,
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("Data: ", data);
+            if (data.status === "success") {
+                console.log("Response:", data);
+                setUploadURL(data.url);
+            }
+        });
 };
